0f30a7b0e0f01d3cf950385efb783010cfee5731
usa8bit <usa8bit@gmail.com>
2023-01-30 20:05:26 +0700

    [ssi-bad-debt-allowance] 14.0.1.0.0

---
+++ ssi_bad_debt_allowance/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    bad_debt_allowance,
+    bad_debt_allowance_detail,
+    bad_debt_allowance_type,
+    bad_debt_allowance_type_percentage,
+)
